763635fb96882562eb6a11ddf675356d315301a8
Jessica Kerr <jessitron@honeycomb.io>
2024-01-07 19:20:03 -0600

    failing test for saved spans

--- test/tracing/BoothGameProcessorDoubleSend.test.ts
+++ test/tracing/BoothGameProcessorDoubleSend.test.ts
@@ -1,3 +1,4 @@
+import { BOOTH_GAME_TELEMETRY_DESTINATION } from "../../src/tracing/BoothGameProcessor";
 import {
   boothGameProcessor,
   normalProcessor,
@@ -31,6 +32,7 @@ describe("booth game processor sending to the customer's team", () => {
     expect(customerApiKey).toEqual("fake api key");
 
     boothGameProcessor.clearCustomerTeam(); // for the next test
+    normalProcessor.clearMemory();
   });
 
   test("When it has a customer processor, every span goes there WITH the team attributes set", () => {
@@ -40,7 +42,6 @@ describe("booth game processor sending to the customer's team", () => {
       environment: { slug: "quiz-local" },
       apiKey: "fake api key",
     });
-    normalProcessor.clearMemory();
 
     expect(customerProcessor).toBeDefined();
 
@@ -63,13 +64,67 @@ describe("booth game processor sending to the customer's team", () => {
     // expect(customerSpan?.status).toEqual(normalSpan.status); this won't be true, hrm, I wonder if I can send the ended span to both most of the time
 
     boothGameProcessor.clearCustomerTeam(); // for the next test
+    normalProcessor.clearMemory();
   });
 
-  test("After the customer API key is cleared, it stops sending anything to the customer processor", () => {});
+  test("After the customer API key is cleared, it stops sending anything to the customer processor", () => {
+    // in real life, this is gonna be too subtle a timing. So I'm not gonna worry much about this test
+  });
 
-  test("Spans sent to the customer processor have the destination set to customer, unlike the ones sent to our team", () => {});
+  test("Spans sent to the customer processor have the destination set to customer, unlike the ones sent to our team", () => {
+    boothGameProcessor.learnCustomerTeam({
+      region: "us",
+      team: { slug: "modernity" },
+      environment: { slug: "quiz-local" },
+      apiKey: "fake api key",
+    });
 
-  test("When it gets spans before the customer processor, and then it gets the customer processor, it sends the spans to the customer processor", () => {});
+    expect(customerProcessor).toBeDefined();
+
+    const span = tracer.startSpan("fake span", { attributes: { testAttribute: "does it care" } });
+
+    span.end();
+    expect(customerProcessor?.endedSpans.length).toEqual(1);
+
+    const customerSpan = customerProcessor?.onlyEndedSpan();
+    expect(customerSpan?.attributes[BOOTH_GAME_TELEMETRY_DESTINATION]).toEqual("customer");
+
+    boothGameProcessor.clearCustomerTeam(); // for the next test
+    normalProcessor.clearMemory();
+  });
+
+  test("When it gets spans before the customer processor, and then it gets the customer processor, it sends the spans to the customer processor", () => {
+    const span = tracer.startSpan("fake span", { attributes: { testAttribute: "does it care" } });
+    span.end();
+
+    expect(normalProcessor.endedSpans.length).toEqual(1); // there it goes, to the normal one
+    expect(customerProcessor).toBeUndefined; // not yet
+
+    boothGameProcessor.learnCustomerTeam({
+      region: "us",
+      team: { slug: "modernity" },
+      environment: { slug: "quiz-local" },
+      apiKey: "fake api key",
+    });
+
+    expect(customerProcessor).toBeDefined();
+
+    expect(customerProcessor?.startedSpans.length).toEqual(1);
+    expect(customerProcessor?.endedSpans.length).toEqual(1);
+
+    const customerSpan = customerProcessor?.onlyEndedSpan();
+    const normalSpan = normalProcessor.onlyEndedSpan();
+
+    expect(customerSpan?.spanContext()).toEqual(normalSpan.spanContext());
+    expect(customerSpan?.duration).toEqual(normalSpan.duration);
+    expect(customerSpan?.startTime).toEqual(normalSpan.startTime);
+    expect(customerSpan?.endTime).toEqual(normalSpan.endTime);
+    expect(customerSpan?.instrumentationLibrary).toEqual(normalSpan.instrumentationLibrary);
+    expect(customerSpan?.name).toEqual(normalSpan.name);
+
+    boothGameProcessor.clearCustomerTeam(); // for the next test
+    normalProcessor.clearMemory();
+  });
 
   test("When it gets a team, then it's cleared, then it gets another team, it sends the spans received in between to the new team", () => {});
 });
