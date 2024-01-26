7c988003c50a8e8cc285bb0c1a346e71095dc80b
Jessica Kerr <jessitron@honeycomb.io>
2024-01-26 11:25:25 -0800

    Disable fs garbage

--- test/fake-backend/fakeTracing.ts
+++ test/fake-backend/fakeTracing.ts
@@ -6,7 +6,13 @@ import { NodeSDK } from "@opentelemetry/sdk-node";
 import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
 
 const sdk = new NodeSDK({
-  instrumentations: [getNodeAutoInstrumentations()],
+  instrumentations: [
+    getNodeAutoInstrumentations({
+      "@opentelemetry/instrumentation-fs": {
+        enabled: false,
+      },
+    }),
+  ],
 });
 
 console.log("starting sdk");
