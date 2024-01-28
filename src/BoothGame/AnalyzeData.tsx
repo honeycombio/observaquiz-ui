157f82bfb91da83004b8ff5b4c42b0db12de2d5e
Jessica Kerr <jessitron@honeycomb.io>
2024-01-28 09:24:22 -0800

    A query to point them to

--- src/BoothGame/AnalyzeData.tsx
+++ src/BoothGame/AnalyzeData.tsx
@@ -16,3 +16,40 @@ export function AnalyzeData() {
     </ComponentLifecycleTracing>
   );
 }
+
+function queryDefinition(trace_id: string) {
+  return `{
+  "time_range": 600,
+  "granularity": 0,
+  "breakdowns": [
+      "app.post_answer.question"
+  ],
+  "calculations": [
+      {
+          "op": "MAX",
+          "column": "duration_ms"
+      }
+  ],
+  "filters": [
+      {
+          "column": "name",
+          "op": "=",
+          "value": "Ask LLM for Response"
+      },
+      {
+          "column": "trace.trace_id",
+          "op": "=",
+          "value": "${trace_id}"
+      }
+  ],
+  "orders": [
+      {
+          "column": "duration_ms",
+          "op": "MAX",
+          "order": "descending"
+      }
+  ],
+  "havings": [],
+  "limit": 1000
+}`;
+}
