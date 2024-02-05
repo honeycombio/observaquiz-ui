0dd82756b5fd9b4bbee5b0376d3d541e2404de4d
Jessica Kerr <jessitron@honeycomb.io>
2024-02-04 20:35:58 -0600

    Add a suggestion box

--- src/BoothGame/Win.tsx
+++ src/BoothGame/Win.tsx
@@ -18,6 +18,13 @@ function WinInternal(props: WinProps) {
     });
   };
 
+  const postSuggestion = (input: string) => {
+    console.log("Posting suggestion", input);
+    activeLifecycleSpan.addLog("Prize Suggestion", {
+      "app.boothGame.suggested_prize": input,
+    });
+  };
+
   return (
     <div>
       <h1>You win!</h1>
@@ -27,14 +34,18 @@ function WinInternal(props: WinProps) {
       </p>
       <p className="score-report">Your score is: {props.score}</p>
       <MonikerForLeaderboard report={postToLeaderboard} />
+      <AskThemAboutPrizes report={postSuggestion} />
+      <p className="fine-print">
+        Also, if you think this app is ugly--ME TOO! Please help us. Find us in #devrel and pair with us.
+      </p>
     </div>
   );
 }
 
-type MonikerForLeaderboardProps = {
-  report: (moniker: string) => void;
+type DoStuffWithInputProps = {
+  report: (input: string) => void;
 };
-function MonikerForLeaderboard(props: MonikerForLeaderboardProps) {
+function MonikerForLeaderboard(props: DoStuffWithInputProps) {
   const [moniker, setMoniker] = React.useState("");
   const [state, setState] = React.useState<MonikerState>(NoMoniker);
 
@@ -77,6 +88,59 @@ function MonikerForLeaderboard(props: MonikerForLeaderboardProps) {
           onKeyUp={submitOnEnter}
         ></input>
       </label>
+      <button disabled={!state.buttonEnabled} type="submit" onClick={submit}>
+        Submit
+      </button>{" "}
+      {state.message}
+    </p>
+  );
+}
+
+function AskThemAboutPrizes(props: DoStuffWithInputProps) {
+  const [suggestion, setSuggestion] = React.useState("");
+  const [state, setState] = React.useState<MonikerState>(NoMoniker);
+
+  const updateSuggestion = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newSuggestion = event.target.value;
+    if (!newSuggestion.trim()) {
+      setState(NoMoniker);
+    } else {
+      setState(EnteringMoniker);
+    }
+    setSuggestion(newSuggestion);
+  };
+
+  const submit = () => {
+    if (!state.buttonEnabled) {
+      // they could hit enter on a blank input, for instance
+      return;
+    }
+    setState(ScoreSubmitted);
+    props.report(suggestion);
+  };
+
+  const submitOnEnter = (event: React.KeyboardEvent) => {
+    if (event.key === "Enter") {
+      submit();
+    }
+  };
+
+  return (
+    <p>
+      <label htmlFor="suggestion">
+        If this were a DevOpsDays, we would offer a toy to entice you to complete this quiz. In the $10-15 range, what's
+        a toy that would make you want to do this? We don't plan on Honeycomb-branding this prize, you get plenty of
+        that in the quiz itself. Please share suggestions here:
+      </label>
+      <input
+        type="textarea"
+        id="suggestion"
+        value={suggestion}
+        disabled={!state.inputEnabled}
+        onChange={updateSuggestion}
+        className="moniker-input"
+        onKeyUp={submitOnEnter}
+      ></input>
       <button disabled={!state.buttonEnabled} type="submit" onClick={submit}>
         Submit
       </button>{" "}
