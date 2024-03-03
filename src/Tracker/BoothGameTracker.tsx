2b36fda7d676a89acf4a702234bbcdb5606f302e
Jessica Kerr <jessitron@honeycomb.io>
2024-03-03 13:19:29 -0600

    Refactor: start on tracking substeps

--- src/Tracker/BoothGameTracker.tsx
+++ src/Tracker/BoothGameTracker.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 import { ComponentLifecycleTracing } from "../tracing/ComponentLifecycleTracing";
-import { isComplete, TrackedSteps } from "./trackedSteps";
+import { isComplete, TrackedStep, TrackedSteps } from "./trackedSteps";
 import { TracedState, useTracedState } from "../tracing/TracedState";
+
+function paintSteps(steps: TrackedStep[], currentStepPath: string) {
+  return steps.map((step, index) => {
+    const className =
+      step.id === currentStepPath.split("/")[0] // for right now
+        ? "you-are-here"
+        : isComplete(step)
+          ? "completed-step"
+          : "incomplete-step";
+    return <div key={step.id} title={step.name} className={className} ></div>;
+  });
+}
 
 function BoothGameTrackerInternal(props: BoothGameTrackerProps) {
   const { trackedSteps } = props;
@@ -9,15 +21,7 @@ function BoothGameTrackerInternal(props: BoothGameTrackerProps) {
     "app.tracker.currentStep": ts.currentStepPath,
   }));
 
-  const paintedSteps = steps.map((step, index) => {
-    const className =
-      step.id === currentStepPath.split("/")[0] // for right now
-        ? "you-are-here"
-        : isComplete(step)
-        ? "completed-step"
-        : "incomplete-step";
-    return <div key={step.id} title={step.name} className={className} />;
-  });
+  const paintedSteps = paintSteps(steps, currentStepPath)
   return <div id="booth-game-tracker">{paintedSteps}</div>;
 }
 
