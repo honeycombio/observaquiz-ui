7f994d74b5f576e849624a711ca711bf1fd64e33
Jessica Kerr <jessitron@honeycomb.io>
2024-04-11 13:34:12 -0500

    refactor: cleanup

--- src/Configuration.ts
+++ src/Configuration.ts
@@ -8,12 +8,8 @@ export const FakeHoneycomb = {
   honeycomb_auth_url: "/fake-hny-auth",
 };
 
-export const RealHoneycomb = {
-  honeycomb_auth_url: "https://api.honeycomb.io/1/auth",
-};
-
 type ConfigurationType = {
   honeycomb_auth_url: string;
 };
 
-export const Configuration = React.createContext<ConfigurationType>(RealHoneycomb);
+export const Configuration = React.createContext<ConfigurationType>(Production);
