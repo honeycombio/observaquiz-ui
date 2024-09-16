9aba1bdb5e4a3c96e326d90508146d91057e6bcc
Jessica Kerr <jessitron@honeycomb.io>
2024-09-15 20:20:19 -0600

    Oops committed airplane mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { initializeTelemetry } from "./tracing/tracing"; // TODO: pass configuration to initialization
 import { Configuration, Airplane, Production, Test } from "./Configuration";
-const configuration = Airplane;
+const configuration = Production;
 const learnTeam = initializeTelemetry(configuration);
 import React from "react";
 import { createRoot } from "react-dom/client";
